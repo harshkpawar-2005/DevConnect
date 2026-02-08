53256eddac2ff26f6d0271fcc2dcbd285542e0a3
Harshal Pawar <harshkpawar2005@gmail.com>
2026-02-08 20:48:32 +0530

    Implement refresh token functionality and update user routes to include refresh-token endpoint

--- Backend/src/routes/user.route.js
+++ Backend/src/routes/user.route.js
@@ -1,5 +1,5 @@
 import router from "express";
-import { loginUser, logoutUser, registerUser } from "../controllers/user.controller";
+import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controller";
 import { verifyJWT } from "../middlewares/auth.middleware";
 
 const userRouter= router.Router()
@@ -10,5 +10,7 @@ userRouter.route("/login").post(loginUser)
 //protected route
 userRouter.route("/logout").post(verifyJWT,logoutUser)
 
+//if aceess token expires, use refresh token to get new access token
+userRouter.route("/refresh-token").post(refreshAccessToken)
 
 export { userRouter }
